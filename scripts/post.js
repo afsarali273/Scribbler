f175e302382009767b6b6cc906dcddf4cf039417
Afsar Ali <afsarali273@gmail.com>
2021-05-10 01:05:09 +0800

    add post page

--- scripts/post.js
+++ scripts/post.js
@@ -1,91 +1,67 @@
-// const queryString = new Array();
-// let editMode = false;
-// let num = 0;
+showPost();
+let likes = 0;
 
-// function onEdit() {
-//         num += 1;
-//         if (!editMode) {
-//                 document.getElementById('edit-heading').style.borderWidth = '2px';
-//                 document.getElementById('edit-heading').style.borderStyle = 'solid';
-//                 document.getElementById('edit-heading').style.borderColor = 'red';
+// load content from session storage
+function showPost() {
+  document
+    .getElementById("title")
+    .setAttribute("value", sessionStorage.getItem("title"));
 
-//                 document.getElementById('edit-contenttext').style.borderWidth = '2px';
-//                 document.getElementById('edit-contenttext').style.borderStyle = 'solid';
-//                 document.getElementById('edit-contenttext').style.borderColor = 'red';
+  document.getElementById("name2").innerHTML = sessionStorage.getItem(
+    "userName"
+  );
 
-//                 document.getElementById('edit-button').innerHTML =
-//                         'Save<i class="fa fa-save" style="padding-left: 4px;"></i></button>';
-//                 editMode = true;
-//         } else {
-//                 if (num === 2) {
-//                         var temp = document.getElementById('edit-heading');
-//                         document.getElementById('edit-heading').innerHTML = `<span>UPDATED:</span>${temp.innerHTML}`;
+  document.getElementById("postContent").innerHTML = sessionStorage
+    .getItem("postContent")
+    .replace(/\s+/g, " ")
+    .trim();
+}
 
-//                         var temp = document.getElementById('edit-contenttext');
-//                         document.getElementById('edit-contenttext').innerHTML = `<div>UPDATED:</div>${temp.innerHTML}`;
-//                 }
+// allow editing of post title and content
+function edit() {
+  let title = document.getElementById("title");
+  title.style.border = "1px solid pink";
+  title.removeAttribute("readonly");
 
-//                 document.getElementById('edit-contenttext').style.border = 'none';
-//                 document.getElementById('edit-heading').style.border = 'none';
+  let postContent = document.getElementById("postContent");
+  postContent.style.border = "1px solid pink";
+  postContent.setAttribute("contentEditable", true);
 
-//                 document.getElementById('edit-button').innerHTML =
-//                         'Edit<i class="fa fa-edit" style="padding-left: 4px;"></i>';
-//                 document.getElementById('edit-button').disabled = true;
-//         }
-// }
+  document.getElementById("edit").style.display = "none";
+  document.getElementById("save").style.display = "";
+}
 
-// window.onload = function() {
-//         if (queryString.length == 0) {
-//                 if (window.location.search.split('?').length > 1) {
-//                         const params = window.location.search.split('?')[1].split('&');
-//                         for (let i = 0; i < params.length; i++) {
-//                                 const key = params[i].split('=')[0];
-//                                 const value = decodeURIComponent(params[i].split('=')[1]);
-//                                 queryString[key] = value;
-//                         }
-//                 }
-//         }
-//         if (queryString.heading != null && queryString.author != null) {
-//                 const { heading } = queryString;
-//                 const { author } = queryString;
-//                 const { content } = queryString;
-//                 document.getElementsByClassName('heading-content')[0].innerHTML = heading;
-//                 document.getElementsByClassName('author-name')[0].innerHTML = author;
-//                 document.getElementsByClassName('post-content')[0].innerHTML = content;
-//         }
-//         document.getElementById('comments').style.visibility = 'hidden';
-// };
+// save changes to post
+function save() {
+  let title = document.getElementById("title");
+  title.style.border = "none";
+  title.setAttribute("readOnly", true);
 
-// let count = 0;
-// function countLikes() {
-//         count = parseInt(count) + parseInt(1);
-//         const divData = document.getElementById('showCount');
-//         const likeButton = document.getElementById('likeButton');
-//         likeButton.innerHTML = 'Liked';
-//         if (count == 1) {
-//                 divData.innerHTML = `${count} person likes this !`;
-//         } else {
-//                 divData.innerHTML = `${count} people like this !`;
-//         }
-// }
+  let postContent = document.getElementById("postContent");
+  postContent.style.border = "none";
+  postContent.setAttribute("contentEditable", false);
 
-// const comments = [];
+  document.getElementById("save").style.display = "none";
+  document.getElementById("edit").style.display = "";
+}
 
-// function addingComment(item, index) {
-//         const temp = document.getElementById('comments').innerHTML;
-//         document.getElementById('comments').innerHTML = `<div class="comment">${item}</div>`;
-//         // + `</br>`;
-//         document.getElementById('comments').innerHTML += `${temp}</br>`;
-//         comments.pop();
-// }
+// add likes to post
+function like() {
+  likes++;
+  document.querySelector(".like button").innerHTML =
+    '<i class="fas fa-thumbs-up"></i> &nbsp; Liked';
+  if (likes >= 1) {
+    document.querySelector(".like p").innerHTML = likes + " person likes this!";
+  } else {
+    document.querySelector(".like p").innerHTML = likes + " people like this!";
+  }
+}
 
-// function addComment(comment) {
-//         if (comment.value !== '') {
-//                 document.getElementById('comments').style.visibility = 'visible';
-//                 var comment = comment.value;
-//                 comments.unshift(comment);
-//                 $('#comment').val('');
-//                 comments.forEach(addingComment);
-//                 $(document).scrollTop($(document).height());
-//         }
-// }
+// post comments in chronological order
+function postComment() {
+  let comment = document.getElementById("comment");
+  let commentList = document.getElementById("all-comments");
+  commentList.innerHTML =
+    "<p>" + comment.value + "</p>" + commentList.innerHTML;
+  comment.value = "";
+}
